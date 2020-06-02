680ecb55fe4f6b0744966ef60783426cd97fa9dc
BTHieu <20161498@student.hust.edu.vn>
2020-06-03 00:22:49 +0700

    Demo giao diện (chạy được fake data)

--- src/calculator/HalsteadCalculator.java
+++ src/calculator/HalsteadCalculator.java
@@ -5,23 +5,85 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class HalsteadCalculator {
-	public static void main(String[] args) {
+//	public static void main(String[] args) {
+//		
+//		BufferedReader br = null;
+//		BufferedReader br2 = null;
+//		try {
+//			br = new BufferedReader(new FileReader("F:\\CityGuide.java"));
+//			br2 = new BufferedReader(new FileReader("F:\\CityGuide.java"));
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		
+//		CountOprerator countOprerator = new CountOprerator();
+//		int N1 = countOprerator.countN1(br);
+//		int n1 = countOprerator.countn1(br2);
+//				
+//		System.out.println("Tong so toan tu N1 = "+N1);
+//		System.out.println("So toan tu n1 = "+n1);
+//	}
+	
+	public void calculate(int n1, int n2, int N1, int N2) {
 		
-		BufferedReader br = null;
-		BufferedReader br2 = null;
-		try {
-			br = new BufferedReader(new FileReader("F:\\CityGuide.java"));
-			br2 = new BufferedReader(new FileReader("F:\\CityGuide.java"));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		CountOprerator countOprerator = new CountOprerator();
-		int N1 = countOprerator.countN1(br);
-		int n1 = countOprerator.countn1(br2);
-				
-		System.out.println("Tong so toan tu N1 = "+N1);
-		System.out.println("So toan tu n1 = "+n1);
 	}
+	
+	public float calculate_N(int N1, int N2) {
+		// N (Program Length) = N1 + N2 
+		return (float) N1 + N2;
+	}
+	
+	public float calculate_n(int n1, int n2) {
+		// n (Program Vocabulary) = n1 + n2 
+		return (float) n1 + n2;
+	}
+	
+	public float calculate_H(int n1, int n2) {
+		// H (Halstead Program Length) = n1 * log2(n1) + n2 * log2(n2)
+		return (float) n1*log2(n1) + n2*log2(n2);
+	}
+	
+	public float calculate_V(float N, float n) {
+		// V (Program Volume) = N * log2(n)
+		return (float) N * log2(n);
+	}
+	
+	public float calculate_D(int n1, int n2, float N2) {
+		// D (Program Difficulty) = (n1 / 2) * (N2 / n2)
+		return (float) (n1 / 2) * (N2 / n2);
+	}
+	
+	public float calculate_L(float D) {
+		// L (Program Level) = 1 / D
+		return (float) 1 / D;
+	}
+	
+	public float calculate_E(float D, float V) {
+		// E (Effort) = D * V
+		return (float) D * V;
+	}
+	
+	public float calculate_T(float E) {
+		// T (Time to program) = E / 18
+		return (float) E / 18.0f;
+	}
+	
+	public float calculate_B_FirstWay(float E) {
+		// B (Number of delivered bugs) = E ^ (2/3) / 3000
+		return (float) Math.pow(E, 2/3) / 3000.0f;
+	}
+	
+	public float calculate_B_SecondWay(float V) {
+		// B (Number of delivered bugs) = V / 3000
+		return (float) V / 3000.0f;
+	}
+	
+	// Utility
+	
+	private static float log2(float x) {
+		return (float) (Math.log(x) / Math.log(2));
+	}
+	
+	
 }
--- src/gui/MainScreen.java
+++ src/gui/MainScreen.java
@@ -1,32 +1,75 @@
 package gui;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.SwingConstants;
 import java.awt.Font;
-import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
-import javax.swing.JTextField;
-import javax.swing.JComboBox;
 import javax.swing.JButton;
-import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+import calculator.HalsteadCalculator;
 
 public class MainScreen extends JFrame {
-
+	
+	private static final long serialVersionUID = 1L;
+	
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
+	
+//	private JTextField textField;
+//	private JTextField textField_1;
+//	private JTextField textField_2;
+//	private JTextField textField_3;
+//	private JTextField textField_4;
+	
+	private JLabel headerLabel;
+
+	private JLabel basicParamLabel;	
+	private JLabel param_n1_Label;
+	private JLabel param_n2_Label;
+	private JLabel param_N1_Label;
+	private JLabel param_N2_Label;
+	
+	private JTextField param_n1_TextField;
+	private JTextField param_n2_TextField;
+	private JTextField param_N1_TextField;
+	private JTextField param_N2_TextField;
+	
+	private JLabel chooseFileLabel;
+	private JTextField chooseFileTextField;
+	
+	private JButton chooseButton;
+	private JButton calculateButton;
+	
+	private JLabel metricLabel;
+	private JTextArea metricTextArea;
+	
+	private JLabel recommendationLabel;
+	private JTextField recommendationTextField;
+	
+	public int value_n1 = 8;
+	public int value_n2 = 16;
+	public int value_N1 = 50;
+	public int value_N2 = 70;
+	
+	private float result_N;
+	private float result_n;
+	private float result_H;
+	private float result_V;
+	private float result_D;
+	private float result_L;
+	private float result_E;
+	private float result_T;
+	private float result_B;
 
 	/**
 	 * Launch the application.
@@ -48,140 +91,205 @@ public class MainScreen extends JFrame {
 	 * Create the frame.
 	 */
 	public MainScreen() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 863, 525);
+		setBounds(100, 100, 900, 550);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
-		JLabel lblNewLabel = new JLabel("HALSTEAD CALCULATOR");
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		headerLabel = new JLabel("HALSTEAD CALCULATOR");
+		headerLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
+		headerLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		
-		JLabel lblInsertParameters = new JLabel("Basic Parameters");
-		lblInsertParameters.setFont(new Font("Tahoma", Font.BOLD, 11));
+		basicParamLabel = new JLabel("Basic Parameters");
+		basicParamLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
 		
-		JLabel lblNewLabel_1 = new JLabel("n1 (seperate operators)");
+		param_n1_Label = new JLabel("n1 (seperate operators)");
+		param_n2_Label = new JLabel("n2 (seperate operands)");
+		param_N1_Label = new JLabel("N1 (total amount of operators)");
+		param_N2_Label = new JLabel("N2 (total amount of operands)");
 		
-		JLabel lblNewLabel_2 = new JLabel("n2 (seperate operands)");
+		param_n1_TextField = new JTextField();
+		param_n1_TextField.setHorizontalAlignment(SwingConstants.RIGHT);
+		param_n1_TextField.setEditable(false);
+		param_n1_TextField.setColumns(10);
 		
-		JLabel lblNewLabel_3 = new JLabel("N1 (total amount of operators)");
+		param_n2_TextField = new JTextField();
+		param_n2_TextField.setHorizontalAlignment(SwingConstants.RIGHT);
+		param_n2_TextField.setEditable(false);
+		param_n2_TextField.setColumns(10);
 		
-		JLabel lblNewLabel_4 = new JLabel("N2 (total amount of operands)");
+		param_N1_TextField = new JTextField();
+		param_N1_TextField.setHorizontalAlignment(SwingConstants.RIGHT);
+		param_N1_TextField.setEditable(false);
+		param_N1_TextField.setColumns(10);
 		
-		textField = new JTextField();
-		textField.setEditable(false);
-		textField.setColumns(10);
+		param_N2_TextField = new JTextField();
+		param_N2_TextField.setHorizontalAlignment(SwingConstants.RIGHT);
+		param_N2_TextField.setEditable(false);
+		param_N2_TextField.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setEditable(false);
-		textField_1.setColumns(10);
+		chooseFileLabel = new JLabel("Choose a Java file");
+		chooseFileLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
+		chooseFileTextField = new JTextField();
+		chooseFileTextField.setColumns(10);
 		
-		textField_2 = new JTextField();
-		textField_2.setEditable(false);
-		textField_2.setColumns(10);
+		chooseButton = new JButton("Choose");
+		chooseButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				chooseFile();
+			}
+		});
 		
-		textField_3 = new JTextField();
-		textField_3.setEditable(false);
-		textField_3.setColumns(10);
+		metricLabel = new JLabel("Metrics");
+		metricLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
 		
-		JLabel lblChooseAJava = new JLabel("Choose a Java file");
-		lblChooseAJava.setFont(new Font("Tahoma", Font.BOLD, 11));
+		recommendationLabel = new JLabel("RECOMMENDATION =>");
+		recommendationLabel.setFont(new Font("Tahoma", Font.BOLD, 11));
 		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
+		recommendationTextField = new JTextField();
+		recommendationTextField.setEditable(false);
+		recommendationTextField.setColumns(10);
 		
-		JButton btnNewButton = new JButton("Choose");
+		metricTextArea = new JTextArea();
+		metricTextArea.setEditable(false);
 		
-		JLabel lblNewLabel_5 = new JLabel("Metrics");
-		lblNewLabel_5.setFont(new Font("Tahoma", Font.BOLD, 11));
-		
-		JLabel lblNewLabel_6 = new JLabel("RECOMMENDATION =>");
-		lblNewLabel_6.setFont(new Font("Tahoma", Font.BOLD, 11));
-		
-		textField_5 = new JTextField();
-		textField_5.setEditable(false);
-		textField_5.setColumns(10);
-		
-		JTextArea textArea = new JTextArea();
-		textArea.setEditable(false);
+		calculateButton = new JButton("CALCULATE HALSTEAD");
+		calculateButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				calculateHalstead();
+				displayResult();
+			}
+		});
+		calculateButton.setBackground(UIManager.getColor("Button.shadow"));
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGap(60)
-					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+					.addComponent(headerLabel, GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
 					.addGap(60))
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGap(20)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-						.addComponent(textArea, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 744, Short.MAX_VALUE)
-						.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
-							.addComponent(lblNewLabel_6)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(metricTextArea, GroupLayout.DEFAULT_SIZE, 839, Short.MAX_VALUE)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(recommendationLabel)
 							.addGap(60)
-							.addComponent(textField_5, GroupLayout.DEFAULT_SIZE, 559, Short.MAX_VALUE))
-						.addComponent(lblNewLabel_5, Alignment.LEADING)
-						.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+							.addComponent(recommendationTextField, GroupLayout.DEFAULT_SIZE, 653, Short.MAX_VALUE))
+						.addComponent(metricLabel)
+						.addGroup(gl_contentPane.createSequentialGroup()
 							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addComponent(lblInsertParameters, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE)
-								.addComponent(lblChooseAJava, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE))
+								.addComponent(basicParamLabel, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE)
+								.addComponent(chooseFileLabel, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE))
 							.addGap(60)
 							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 								.addGroup(gl_contentPane.createSequentialGroup()
 									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-										.addComponent(lblNewLabel_1)
-										.addComponent(lblNewLabel_3))
+										.addComponent(param_n1_Label)
+										.addComponent(param_N1_Label))
 									.addGap(25)
 									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-										.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-										.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+										.addComponent(param_n1_TextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+										.addComponent(param_N1_TextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 									.addGap(37)
 									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-										.addComponent(lblNewLabel_2)
-										.addComponent(lblNewLabel_4))
+										.addComponent(param_n2_Label)
+										.addComponent(param_N2_Label))
 									.addGap(36)
 									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-										.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-										.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+										.addComponent(param_N2_TextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+										.addComponent(param_n2_TextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
 								.addGroup(gl_contentPane.createSequentialGroup()
-									.addComponent(textField_4, GroupLayout.PREFERRED_SIZE, 467, GroupLayout.PREFERRED_SIZE)
-									.addGap(29)
-									.addComponent(btnNewButton)))))
+									.addComponent(chooseFileTextField, GroupLayout.PREFERRED_SIZE, 570, GroupLayout.PREFERRED_SIZE)
+									.addGap(20)
+									.addComponent(chooseButton)))))
 					.addGap(72))
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(370)
+					.addComponent(calculateButton)
+					.addContainerGap(448, Short.MAX_VALUE))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
 					.addGap(7)
-					.addComponent(lblNewLabel)
+					.addComponent(headerLabel)
 					.addGap(28)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblChooseAJava)
-						.addComponent(textField_4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnNewButton))
-					.addGap(40)
+						.addComponent(chooseFileLabel)
+						.addComponent(chooseFileTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(chooseButton))
+					.addGap(21)
+					.addComponent(calculateButton)
+					.addGap(31)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblInsertParameters)
-						.addComponent(lblNewLabel_1)
-						.addComponent(lblNewLabel_2)
-						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(basicParamLabel)
+						.addComponent(param_n1_Label)
+						.addComponent(param_n2_Label)
+						.addComponent(param_n1_TextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(param_n2_TextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(18)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel_3)
-						.addComponent(lblNewLabel_4)
-						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(param_N1_Label)
+						.addComponent(param_N2_Label)
+						.addComponent(param_N1_TextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(param_N2_TextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(lblNewLabel_5)
+					.addComponent(metricLabel)
 					.addGap(18)
-					.addComponent(textArea, GroupLayout.PREFERRED_SIZE, 192, GroupLayout.PREFERRED_SIZE)
+					.addComponent(metricTextArea, GroupLayout.PREFERRED_SIZE, 192, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED, 29, Short.MAX_VALUE)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel_6)
-						.addComponent(textField_5, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+						.addComponent(recommendationLabel)
+						.addComponent(recommendationTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(20))
 		);
 		contentPane.setLayout(gl_contentPane);
 	}
+	
+	// Handle events
+	
+	private void chooseFile() {
+		
+	}
+	
+	private void calculateHalstead() {
+		HalsteadCalculator cal = new HalsteadCalculator();
+		
+		result_N = cal.calculate_N(value_N1, value_N2);
+		result_n = cal.calculate_n(value_n1, value_n2);
+		result_H = cal.calculate_H(value_n1, value_n2);
+		result_V = cal.calculate_V(result_N, result_n);
+		result_D = cal.calculate_D(value_n1, value_n2, value_N2);
+		result_L = cal.calculate_L(result_D);
+		result_E = cal.calculate_E(result_D, result_V);
+		result_T = cal.calculate_T(result_E);
+		result_B = cal.calculate_B_SecondWay(result_V);
+	}
+	
+	private void displayResult() {
+		param_n1_TextField.setText(String.valueOf(value_n1));
+		param_n2_TextField.setText(String.valueOf(value_n2));
+		param_N1_TextField.setText(String.valueOf(value_N1));
+		param_N2_TextField.setText(String.valueOf(value_N2));
+		
+		String metricString = String.format("N (Program Length) = %f", result_N) + " \r\n"
+							+ String.format("n (Program Vocabulary) = %f", result_n) + " \r\n"
+							+ String.format("H (Halstead Program Length) = %f", result_H) + " \r\n"
+							+ String.format("V (Program Volume) = %f", result_V) + " \r\n"
+							+ String.format("D (Program Difficulty) = %f", result_D) + " \r\n"
+							+ String.format("L (Program Level) = %f", result_L) + " \r\n"
+							+ String.format("E (Effort) = %f", result_E) + " \r\n"
+							+ String.format("T (Time required to program) = %f", result_T) + " \r\n"
+							+ String.format("B (Number of delivered bugs) = %f", result_B) + " \r\n";
+		//System.out.print(metricString);
+		metricTextArea.setText(metricString);
+		
+		recommendationTextField.setText("Code như shit");
+		
+				
+	}
+
 }
